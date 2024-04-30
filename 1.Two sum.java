540f4d5f9ba685111567eede072aefebd310b31a
Rahul Vanchivel N <114805457+rahulvanchivel@users.noreply.github.com>
2024-04-30 11:13:36 +0530

    Update 1.Two sum.java

--- 1.Two sum.java
+++ 1.Two sum.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n=nums.length;
-        for(int i=0;i<n-1;i++)
+        for(int i=0;i<n-1;i++)//loop
         {
             for(int j=i+1;j<n;j++)
             {
-                if(nums[i]+nums[j]==target)
+                if(nums[i]+nums[j]==target)//condition
                 {
                     return new int[] {i, j};
                 }
